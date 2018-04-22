9bb7f39aaf49f038ac3e4d9138a19e481dd08425
Mike King <michael.king@zeromass.co.uk>
2018-04-22 21:11:21 +0100

    added test code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # technical-hema
 assessment submission for Hema's technical task (csv daily data)
+
+Dependencies:
+None
+
+Installation:
+Copy the solution script and test files (1.csv, 2.csv, 3.csv) to a folder
+
+Running:
+From the installation folder, run:
+python3 solution.py
+
+Tests:
+run python3 solution.py TEST
--- solution.py
+++ solution.py
@@ -1,5 +1,5 @@
 # Technical Task Solution
-#import csv
+import sys
 
 # checked input files for formatting so we can make a few assumptions:
 # line-terminator = '\n' (ASCII 0x0A)
@@ -8,7 +8,7 @@ LINETERM = '\n'
 DELIMITER = ','
 # files contain: one header record (line 0) and one data record (line 1)
 # so, files appear to be conformant with RFC4180 and given simple formatting, we 
-# could forego using the CSV module and directly operate on the data.
+# can forego using the CSV module and directly operate on the data.
 
 # notes:
 # expected output appears to show results in sorted dictionary key order - not explicitly
@@ -70,40 +70,30 @@ class Day(object):
 
 
 # day meta data
+TEST_DATA_1_HDR = ["mon","tue","some_column1","wed","thu","fri","description"]
+TEST_DATA_1_DROW = [1,5,"data",2,3,3,"first_desc"]
+
+TEST_DATA_2_HDR = ["mon-thu","fri","description","another_column2"]
+TEST_DATA_2_DROW = [2,3,"second_desc","some_data"]
+
+TEST_RESULTS_1 = [
+    {'day': 'mon', 'description': 'first_desc 1', 'square': 1, 'value': 1},
+    {'day': 'tue', 'description': 'first_desc 25', 'square': 25, 'value': 5},
+    {'day': 'wed', 'description': 'first_desc 4', 'square': 4, 'value': 2},
+    {'day': 'thu', 'description': 'first_desc 6', 'double': 6, 'value': 3},
+    {'day': 'fri', 'description': 'first_desc 6', 'double': 6, 'value': 3}
+ ]
 
 
-TEST_DATA_1 = """mon,tue,some_column1,wed,thu,fri,description
-1,5,data,2,3,3,first_desc"""
+TEST_OUTPUT_2 = [
+    {'day': 'mon', 'description': 'second_desc 4', 'square': 4, 'value': 2},
+    {'day': 'tue', 'description': 'second_desc 4', 'square': 4, 'value': 2},
+    {'day': 'wed', 'description': 'second_desc 4', 'square': 4, 'value': 2},
+    {'day': 'thu', 'description': 'second_desc 4', 'double': 4, 'value': 2},
+    {'day': 'fri', 'description': 'second_desc 6', 'double': 6, 'value': 3}
+]
 
-TEST_DATA_2 = """mon-thu,fri,description,another_column2
-2,3,second_desc,some_data"""
 
-TEST_DATA_3 = """some_data,mon-tue,wed-thu,fri,description
-,3,2,1,third_desc"""
-
-TEST_OUTPUT_1 = """
-[{'day': 'mon', 'description': 'first_desc 1', 'square': 1, 'value': 1},
- {'day': 'tue', 'description': 'first_desc 25', 'square': 25, 'value': 5},
- {'day': 'wed', 'description': 'first_desc 4', 'square': 4, 'value': 2},
- {'day': 'thu', 'description': 'first_desc 6', 'double': 6, 'value': 3},
- {'day': 'fri', 'description': 'first_desc 6', 'double': 6, 'value': 3}]
-"""
-
-TEST_OUTPUT_2 = """
-[{'day': 'mon', 'description': 'second_desc 4', 'square': 4, 'value': 2},
- {'day': 'tue', 'description': 'second_desc 4', 'square': 4, 'value': 2},
- {'day': 'wed', 'description': 'second_desc 4', 'square': 4, 'value': 2},
- {'day': 'thu', 'description': 'second_desc 4', 'double': 4, 'value': 2},
- {'day': 'fri', 'description': 'second_desc 6', 'double': 6, 'value': 3}]
-"""
-TEST_OUTPUT_3 = """
-[{'day': 'mon', 'description': 'third_desc 9', 'square': 9, 'value': 3},
- {'day': 'tue', 'description': 'third_desc 9', 'square': 9, 'value': 3},
- {'day': 'wed', 'description': 'third_desc 4', 'square': 4, 'value': 2},
- {'day': 'thu', 'description': 'third_desc 4', 'double': 4, 'value': 2},
- {'day': 'fri', 'description': 'third_desc 2', 'double': 2, 'value': 1}]
-
-"""
 
 def read_csv(filename):
 # read_csv(file_name)
@@ -132,7 +122,6 @@ def process_files(flist):
         res=process_file(filename)
         results[filename]=res
     return results
-
 
 def process_file(filename):
     headings,data_rows = read_csv(filename)
@@ -194,16 +183,34 @@ def get_daily_data(headings,data_row):
     
     return final
 
-
-def main():
+def tests():
+    test_res = get_daily_data(TEST_DATA_1_HDR, TEST_DATA_1_DROW)
+    assert(len(test_res) == len(TEST_RESULTS_1))
+    
+    print("All tests passed")
+    
+def standard_run():    
     filenames = ['1.csv','2.csv','3.csv']
 
     res=process_files(filenames)
     for filename,daily_data in res.items():
         print(filename)
         for day in daily_data:
-            print(day, sep='\n')
+            print(day)  #, sep='\n')
     #print(res)
+
+
+def main():
+    if len(sys.argv)>1:
+        run_mode = sys.argv[1].lower().strip()
+        if run_mode == "test":
+            tests()
+            return
+
+    standard_run()
+
+
+
 
 if __name__ == "__main__":
     main()
